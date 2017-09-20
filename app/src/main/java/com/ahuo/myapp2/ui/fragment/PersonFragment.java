7fb42f426c1f994d76eb4271e2f33de46f1700e9
liuhuijie <857166619@qq.com>
2017-09-20 15:24:29 +0800

    添加工具类

--- app/src/main/java/com/ahuo/myapp2/ui/fragment/PersonFragment.java
+++ app/src/main/java/com/ahuo/myapp2/ui/fragment/PersonFragment.java
@@ -1,14 +1,23 @@
 package com.ahuo.myapp2.ui.fragment;
 
+import android.widget.ImageView;
+
 import com.ahuo.myapp2.R;
 import com.ahuo.myapp2.base.BaseTitleFragment;
+import com.ahuo.myapp2.core.AppConfig;
 import com.ahuo.myapp2.ui.widget.MyAppBar;
+import com.ahuo.util.imageloader.GlideLoaderUtil;
+
+import butterknife.BindView;
 
 /**
  * Created by ahuo on 17-9-19.
  */
 
 public class PersonFragment extends BaseTitleFragment {
+    @BindView(R.id.ivUserPhoto)
+    ImageView mIvUserPhoto;
+
     @Override
     public void setPresenter() {
 
@@ -26,11 +35,13 @@ public class PersonFragment extends BaseTitleFragment {
 
     @Override
     public void initData() {
-
+        GlideLoaderUtil.loadNormalImage(getActivity(), AppConfig.APP_LOGO, GlideLoaderUtil.LOAD_IMAGE_DEFAULT_ID, mIvUserPhoto);
     }
 
     @Override
     public void refresh() {
 
     }
+
+
 }
